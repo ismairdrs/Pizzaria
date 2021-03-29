b3eb2a644c2cdc6218e0478f25797e3b6ef41e86
ismairdrs <ismairdrs@outlook.com>
2021-03-28 21:04:32 -0300

    atualizando arquivos para deploy no heroku

--- pizzaria/settings.py
+++ pizzaria/settings.py
@@ -15,15 +15,14 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+from decouple import config
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+from dj_database_url import parse as dburl
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$n+d%9u)%x%w=o169#jhb26ffov&m*spqh_p4-irqf0ubmd$x%'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 ALLOWED_HOSTS = []
 
@@ -135,5 +134,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
