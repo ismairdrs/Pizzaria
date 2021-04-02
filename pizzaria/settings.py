3615ec0a6657cbd4e12c741e77f494041dbedfe9
ismairdrs <ismairdrs@outlook.com>
2021-04-01 22:05:44 -0300

    removendo gunicorn do requirements

--- pizzaria/settings.py
+++ pizzaria/settings.py
@@ -27,8 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework_simplejwt',
+
     'rest_framework',
+    'rest_framework_simplejwt',
     'core',
 ]
 
