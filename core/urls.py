7101be56f8dae41ec006f2d8602c108b3ac90245
ismairdrs <ismairdrs@outlook.com>
2021-04-01 22:15:01 -0300

    fix

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,4 @@ from .api.v1.views import PizzaListView, PizzaCreateView, PizzaDetailView
 
 urlpatterns = [
     url('api/', include(urls)),
-    path('', PizzaListView.as_view(), name='pizza-list'),
-    path('nova-pizza/', PizzaCreateView.as_view(), name='nova-pizza'),
-    path('detalhe-pizza/<int:pk>/', PizzaDetailView.as_view(), name='detail-pizza')
 ]
