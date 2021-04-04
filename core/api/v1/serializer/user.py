b40785e52a8baad7ebdfa0bb9352707c46331f5d
ismairdrs <ismairdrs@outlook.com>
2021-04-04 09:30:20 -0300

    fix

--- core/api/v1/serializer/user.py
+++ core/api/v1/serializer/user.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 
 from core.models.usuario import User
@@ -8,3 +9,12 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'first_name', 'last_name', 'username', 'email')
+
+    def validate_password(self, value: str) -> str:
+        """
+        Hash value passed by user.
+
+        :param value: password of a user
+        :return: a hashed version of the password
+        """
+        return make_password(value)
