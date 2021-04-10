b8876d90e15025b1e340fb8879cba579a622506e
ismairdrs <ismairdrs@outlook.com>
2021-04-09 23:14:26 -0300

    fix

--- core/api/v1/urls.py
+++ core/api/v1/urls.py
@@ -20,18 +20,12 @@ router.register('usuario', UserViewSet, basename='usuario')
 
 
 urlpatterns = [
-    path('pizza/', PizzaListView.as_view(), name='list_pizza'),
-    path('nova-pizza/', PizzaCreateView.as_view(), name='new_pizza'),
-    path('detalhe-pizza/<int:pk>', PizzaDetailView.as_view(), name='detail_pizza'),
 
     path('accounts/', include('django.contrib.auth.urls')),
     path('cadastro', UserCreateView.as_view(), name='new_user'),
     path('usuario/<int:pk>', UserDetailView.as_view(), name='detail_user'),
     path('usuario/<int:pk>/delete', UserDeleteView.as_view(), name='delete-user'),
     path('usuario/<int:pk>/atualizar', UserUpdateView.as_view(), name='new_user'),
-
-    path('avaliacao/', cadastrar_avaliacao, name='avaliacao'),
-
 
 
     url('', include(router.urls)),
