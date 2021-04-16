2216bf479004ce4a479738d726a8f797a390951a
ismairdrs <ismairdrs@outlook.com>
2021-04-16 19:08:07 -0300

    fix

--- core/api/v1/views/likes.py
+++ core/api/v1/views/likes.py
@@ -16,7 +16,7 @@ class Likes(APIView):
         body = self.valida_dados(request)
         if body:
             producer = Producer()
-            producer.produce(exchange='task_queue', body=body, routing_key='task_queue')
+            producer.produce(exchange='likes', body=body, routing_key='likes')
             producer.close_connection()
             return Response({'Likes': 'like recebido'}, status=status.HTTP_202_ACCEPTED)
         else:
