129042b2e57e3186a9ceeeb74fa899302189f3be
ismairdrs <ismairdrs@outlook.com>
2021-04-26 17:22:31 -0300

    incluindo permission no usuario e pizza

--- core/api/v1/views/pizza.py
+++ core/api/v1/views/pizza.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 
 from core.api.v1.serializer.pizza import PizzaSerializer
 from core.models import Pizza
@@ -8,4 +8,10 @@ from core.models import Pizza
 class PizzaViewSet(viewsets.ModelViewSet):
     queryset = Pizza.objects.all()
     serializer_class = PizzaSerializer
-    #permission_classes = (IsAuthenticated, IsAdminUser)
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            self.permission_classes = []
+        else:
+            self.permission_classes = [IsAuthenticated, ]
+        return super(PizzaViewSet, self).get_permissions()
--- core/api/v1/views/usuario.py
+++ core/api/v1/views/usuario.py
@@ -1,11 +1,21 @@
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet
-
+from rest_framework import generics, mixins
 from core.api.v1.serializer.user import UserSerializer
 from core.models import User
 
 
-class UserViewSet(ModelViewSet):
+class UserViewSet(mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.DestroyModelMixin,
+                   generics.GenericViewSet):
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsAuthenticated, )
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            self.permission_classes = []
+        else:
+            self.permission_classes = [IsAuthenticated, ]
+        return super(UserViewSet, self).get_permissions()
