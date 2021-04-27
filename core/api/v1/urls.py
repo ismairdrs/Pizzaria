462f52bf9625f926f7f4ed83f6b4c7f85448cb6c
ismairdrs <ismairdrs@outlook.com>
2021-04-27 10:46:55 -0300

    testando sentry

--- core/api/v1/urls.py
+++ core/api/v1/urls.py
@@ -5,10 +5,18 @@ from rest_framework import routers
 from core.api.v1.views.api_gateway import Gateway
 from core.api.v1.views.likes import Likes
 from core.api.v1.views.pizza import PizzaViewSet
+from core.api.v1.views.teste_websocket import websocket
 from core.api.v1.views.token import TokenObtainPairView
 from core.api.v1.views.usuario import UserViewSet
 
 from rest_framework_simplejwt import views as jwt_views
+
+
+
+from django.urls import path
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
 
 
 
@@ -18,9 +26,12 @@ router.register('usuario', UserViewSet, basename='usuario')
 
 
 urlpatterns = [
+    path('sentry-debug/', trigger_error),
+    path('websocket/', websocket, name='websocket'),
     url('', include(router.urls)),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('avaliacao/', Likes.as_view(), name='avaliacao'),
-    url(r'.*', Gateway.as_view()),
+
+    #url(r'.*', Gateway.as_view()),
 ]
