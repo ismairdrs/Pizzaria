a3b73b3f57945e2b78181fa9cd713712fb07f59e
ismairdrs <ismairdrs@outlook.com>
2021-05-16 10:51:13 -0300

    modificando auth

--- pizzaria/settings.py
+++ pizzaria/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        #'rest_framework.authentication.SessionAuthentication',
-        #'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     #'DEFAULT_RENDERER_CLASSES': (
